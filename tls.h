1f0439f792f3561110db6060d0a8bdd9eec5c343
Andrew McKinlay <apmckinlay@gmail.com>
2007-02-10 19:40:19 +0000

    added GPL and ifndef
    git-svn-id: svn://svn.code.sf.net/p/suneido/svn/trunk@14 8ef86ec9-3220-0410-98d7-ddd3c8784f88

--- tls.h
+++ tls.h
@@ -1,3 +1,28 @@
+#ifndef TLS_H
+#define TLS_H
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+ * This file is part of Suneido - The Integrated Application Platform
+ * see: http://www.suneido.com for more information.
+ * 
+ * Copyright (c) 2006 Suneido Software Corp. 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation - version 2. 
+ *
+ * This program is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License in the file COPYING
+ * for more details. 
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 void register_tls(void** pvar);
 
 struct Tls
@@ -9,3 +34,5 @@ struct Tls
 	};
 
 #define TLS(var) Tls tls_##var(reinterpret_cast<void**>(&var))
+
+#endif
