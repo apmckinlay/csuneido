e072074285ff4a091ba68f7aa1b3d7ae69045db6
Andrew McKinlay <apmckinlay@gmail.com>
2007-05-13 21:43:56 +0000

    changed destroy to drop, added "lower" indexes
    git-svn-id: svn://svn.code.sf.net/p/suneido/svn/trunk@38 8ef86ec9-3220-0410-98d7-ddd3c8784f88

--- qscanner.cpp
+++ qscanner.cpp
@@ -46,14 +46,14 @@ static Keyword words[] =
 	{ "alter", K_ALTER }, { "and", T_AND },
 	{ "average", K_AVERAGE }, { "by", K_BY }, { "cascade", K_CASCADE },
 	{ "count", K_COUNT }, { "create", K_CREATE }, { "delete", K_DELETE }, 
-	{ "destroy", K_DESTROY },
+	{ "destroy", K_DROP }, { "drop", K_DROP },
 	{ "ensure", K_ENSURE }, { "extend", K_EXTEND }, 
 	{ "false", K_FALSE }, { "in", K_IN },
 	{ "index", K_INDEX }, { "insert", K_INSERT }, 
 	{ "intersect", K_INTERSECT }, { "into", K_INTO }, 
 	{ "is", I_IS }, { "isnt", I_ISNT },
 	{ "join", K_JOIN }, { "key", K_KEY },
-	{ "leftjoin", K_LEFTJOIN }, { "list", K_LIST },
+	{ "leftjoin", K_LEFTJOIN }, { "list", K_LIST }, { "lower", K_LOWER },
 	{ "max", K_MAX }, { "min", K_MIN }, { "minus", K_DIFFERENCE }, 
 	{ "not", I_NOT }, { "or", T_OR }, { "project", K_PROJECT },
 	{ "remove", K_REMOVE }, { "rename", K_RENAME }, { "reverse", K_REVERSE },
@@ -92,17 +92,17 @@ void QueryScanner::insert(const gcstring& s)
 #include "except.h"
 
 static char* qscanner_input = "alter average by count create delete \
-		destroy ensure extend false in into index insert intersect join \
-		key leftjoin list max min minus project remove rename reverse set \
+		destroy drop ensure extend false in into index insert intersect join \
+		key leftjoin list lower max min minus project remove rename reverse set \
 		sort summarize times to total true union unique update view where";
 
 static short results[] =
 	{
 	K_ALTER, K_AVERAGE, K_BY,
 	K_COUNT, K_CREATE, K_DELETE, 
-	K_DESTROY, K_ENSURE, K_EXTEND, K_FALSE,
+	K_DROP, K_DROP, K_ENSURE, K_EXTEND, K_FALSE,
 	K_IN, K_INTO, K_INDEX, K_INSERT, 
-	K_INTERSECT, K_JOIN, K_KEY, K_LEFTJOIN, K_LIST,
+	K_INTERSECT, K_JOIN, K_KEY, K_LEFTJOIN, K_LIST, K_LOWER,
 	K_MAX, K_MIN, K_DIFFERENCE, K_PROJECT, K_REMOVE,
 	K_RENAME, K_REVERSE, K_SET, K_SORT, K_SUMMARIZE,
 	K_TIMES, K_TO, K_TOTAL, K_TRUE, K_UNION,
