489d0d6c2294f1a46d0625a9533a92fdc9c9bfa4
Andrew McKinlay <apmckinlay@gmail.com>
2007-05-13 21:57:01 +0000

    changed path to make
    git-svn-id: svn://svn.code.sf.net/p/suneido/svn/trunk@49 8ef86ec9-3220-0410-98d7-ddd3c8784f88

--- vc8_release.bat
+++ vc8_release.bat
@@ -2,5 +2,5 @@ call "C:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat" x86
 @echo on
 call "c:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\setenv" /XP32 /RETAIL
 @echo on
-doskey make=make -f makefile.vc8 $*
+doskey make=\mingw\bin\mingw32-make -f makefile.vc8 $*
 @title vc8 release
