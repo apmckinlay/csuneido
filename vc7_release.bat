fd08f29e917ec2b57908326e6b00c796b3b1b0fd
Andrew McKinlay <apmckinlay@gmail.com>
2009-01-21 22:55:23 +0000

    cosmetic
    git-svn-id: svn://svn.code.sf.net/p/suneido/svn/trunk@459 8ef86ec9-3220-0410-98d7-ddd3c8784f88

--- vc7_release.bat
+++ vc7_release.bat
@@ -5,6 +5,6 @@ call "C:\Program Files\Microsoft Visual C++ Toolkit 2003\vcvars32.bat"
 call "c:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\setenv" /XP32 /RETAIL
 @echo on
 @rem need to reverse these or you get the wrong versions
-SET LIB=C:\Program Files\Microsoft Visual C++ Toolkit 2003\lib;C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\Lib
+set LIB=C:\Program Files\Microsoft Visual C++ Toolkit 2003\lib;C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\Lib
 doskey make=make -f makefile.vc7 $*
 @title vc7 release
