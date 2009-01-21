59cfae9bf78beca40151150c693eac0f1a0320da
Andrew McKinlay <apmckinlay@gmail.com>
2009-01-21 23:05:54 +0000

    updated
    git-svn-id: svn://svn.code.sf.net/p/suneido/svn/trunk@465 8ef86ec9-3220-0410-98d7-ddd3c8784f88

--- snapshot.bat
+++ snapshot.bat
@@ -1,5 +1,12 @@
 suneido -dump stdlib
 suneido -dump suneidoc
+suneido -dump imagebook
+call saveaway suneido.db
+suneido -load stdlib
+suneido -load suneidoc
+suneido -load imagebook
 del suneido%1.zip
-zip -9 suneido%1.zip suneido.exe stdlib.su suneidoc.su scilexer.dll libhpdf.dll clucene.dll suneido%1.txt
+zip -9 suneido%1.zip suneido.exe suneido.db scilexer.dll libhpdf.dll clucene.dll
 du -h suneido%1.zip
+del suneido.db
+unsave suneido.db
