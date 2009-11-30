9d9880832e6e7070d827b37a088b008b896e1a91
Andrew McKinlay <apmckinlay@gmail.com>
2009-11-30 16:46:37 +0000

    added translatelanguage
    git-svn-id: svn://svn.code.sf.net/p/suneido/svn/trunk@854 8ef86ec9-3220-0410-98d7-ddd3c8784f88

--- snapshot.bat
+++ snapshot.bat
@@ -1,10 +1,12 @@
 suneido -dump stdlib
 suneido -dump suneidoc
 suneido -dump imagebook
+copy z:\software\suneido\translatelanguage.su
 call saveaway suneido.db
 suneido -load stdlib
 suneido -load suneidoc
 suneido -load imagebook
+suneido -load translatelanguage.su
 del suneido%1.zip
 zip -9 suneido%1.zip suneido.exe suneido.db scilexer.dll libhpdf.dll clucene.dll
 du -h suneido%1.zip
