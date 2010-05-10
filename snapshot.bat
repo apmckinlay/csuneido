78d15b22728a569f4b3de7bd5735d8e01a8ad020
Andrew McKinlay <apmckinlay@gmail.com>
2010-05-10 20:42:23 +0000

    updated
    git-svn-id: svn://svn.code.sf.net/p/suneido/svn/trunk@973 8ef86ec9-3220-0410-98d7-ddd3c8784f88

--- snapshot.bat
+++ snapshot.bat
@@ -8,7 +8,7 @@ suneido -load suneidoc
 suneido -load imagebook
 suneido -load translatelanguage.su
 del suneido%1.zip
-zip -9 suneido%1.zip suneido.exe suneido.db scilexer.dll libhpdf.dll clucene.dll
+zip -9 suneido%1.zip suneido.exe suneido.db scilexer.dll libhpdf.dll clucene.dll hunspell.exe en_US.aff en_US.dic
 du -h suneido%1.zip
 del suneido.db
 unsave suneido.db
