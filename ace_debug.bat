999e7b41a09a3a50ca1e8b8f67d0917883210c9b
Andrew McKinlay <apmckinlay@gmail.com>
2010-07-02 15:22:19 +0000

    old, not used
    git-svn-id: svn://svn.code.sf.net/p/suneido/svn/trunk@1032 8ef86ec9-3220-0410-98d7-ddd3c8784f88

--- ace_debug.bat
+++
@@ -1,4 +0,0 @@
-@if not exist ace_debug mkdir ace_debug
-@if not exist gc6.5\ace_debug mkdir gc6.5\ace_debug
-doskey make=make -f makefile.ace DEBUG=1 $*
-@title mingw ace debug
--- ace_release.bat
+++
@@ -1,4 +0,0 @@
-@if not exist ace_release mkdir ace_release
-@if not exist gc6.5\ace_release mkdir gc6.5\ace_release
-doskey make=make -f makefile.mingw $*
-@title mingw ace release
