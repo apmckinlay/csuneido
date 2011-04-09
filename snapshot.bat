b1686b178592c1afae814599158b93a7418a6aa3
Andrew McKinlay <apmckinlay@gmail.com>
2011-04-09 22:42:59 +0000

    added jsuneido.jar
    git-svn-id: svn://svn.code.sf.net/p/suneido/svn/trunk@1104 8ef86ec9-3220-0410-98d7-ddd3c8784f88

--- snapshot.bat
+++ snapshot.bat
@@ -7,8 +7,10 @@ suneido -load stdlib
 suneido -load suneidoc
 suneido -load imagebook
 suneido -load translatelanguage.su
+copy ..\workspace\jsuneido\jsuneido.jar
 del suneido%1.zip
-zip -9 suneido%1.zip suneido.exe suneido.db scilexer.dll libhpdf.dll clucene.dll hunspell.exe en_US.aff en_US.dic splash.bmp
+zip -9 suneido%1.zip suneido.exe suneido.db scilexer.dll libhpdf.dll clucene.dll hunspell.exe en_US.aff en_US.dic splash.bmp jsuneido.jar
+del jsuneido.jar
 du -h suneido%1.zip
 del suneido.db
 unsave suneido.db
