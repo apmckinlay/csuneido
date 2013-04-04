28e33aa267e4c0171751ceef65877272abc13895
Andrew McKinlay <apmckinlay@gmail.com>
2013-04-04 14:10:12 +0000

    fixed to support XP
    git-svn-id: svn://svn.code.sf.net/p/suneido/svn/trunk@1188 8ef86ec9-3220-0410-98d7-ddd3c8784f88

--- vc11_release.bat
+++ vc11_release.bat
@@ -1,6 +1,11 @@
 @if not exist vc11_release mkdir vc11_release
 @if not exist gc6.5\vc11_release mkdir gc6.5\vc11_release
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+set INCLUDE=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Include;%INCLUDE%
+set PATH=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Bin;%PATH%
+set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Lib;%LIB%
+set CL=/D_USING_V110_SDK71_;%CL%
+set LINK=/SUBSYSTEM:WINDOWS,5.01 %LINK%
 @echo on
 doskey make=make -f makefile.vc11 $*
 @title vc11 release
