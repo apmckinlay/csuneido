e926a0d24c181a030223e2f710fd6ed5f6867bda
Andrew McKinlay <apmckinlay@gmail.com>
2013-06-02 20:25:58 +0000

    added ThreadCount and string.MapN refactored & improved socket code to eliminate crash factored out duplicate code into Buffer
    git-svn-id: svn://svn.code.sf.net/p/suneido/svn/trunk@1211 8ef86ec9-3220-0410-98d7-ddd3c8784f88

---
+++ buffer.cpp
@@ -0,0 +1,82 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+ * This file is part of Suneido - The Integrated Application Platform
+ * see: http://www.suneido.com for more information.
+ * 
+ * Copyright (c) 2013 Suneido Software Corp. 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation - version 2. 
+ *
+ * This program is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License in the file COPYING
+ * for more details. 
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include "buffer.h"
+#include "gc.h"
+#include "except.h"
+#include "gcstring.h"
+#include "minmax.h"
+#include <string.h>
+
+Buffer::Buffer(int n) 
+	: capacity(n), buf(new(noptrs) char[capacity]), used(0)
+	{ }
+
+char* Buffer::ensure(int n)
+	{
+	++n;  // allow for nul for gcstr
+	if (used + n > capacity)
+		buf = (char*) GC_realloc(buf, capacity = max(2 * capacity, capacity + n));
+	return buf + used;
+	}
+
+char* Buffer::alloc(int n)
+	{
+	char* dst = ensure(n);
+	added(n);
+	return dst;
+	}
+
+void Buffer::add(char c)
+	{
+	*alloc(1) = c;
+	}
+
+void Buffer::add(const char* s, int n)
+	{
+	memcpy(alloc(n), s, n);
+	}
+
+void Buffer::add(const gcstring& s)
+	{
+	int n = s.size();
+	memcpy(alloc(n), s.buf(), n);
+	}
+
+// WARNING: remove will invalidate previous gcstr
+void Buffer::remove(int n)
+	{
+	verify(n <= used);
+	memmove(buf, buf + n, used - n);
+	used -= n;
+	}
+
+gcstring Buffer::gcstr()
+	{
+	return gcstring(used, str()); // no alloc
+	}
+
+char* Buffer::str()
+	{
+	buf[used] = 0;
+	return buf;
+	}
---
+++ buffer.h
@@ -0,0 +1,66 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+ * This file is part of Suneido - The Integrated Application Platform
+ * see: http://www.suneido.com for more information.
+ * 
+ * Copyright (c) 2013 Suneido Software Corp. 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation - version 2. 
+ *
+ * This program is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License in the file COPYING
+ * for more details. 
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+class gcstring;
+
+class Buffer
+	{
+public:
+	explicit Buffer(int n = 128);
+
+	// make sure the buffer has space for n more characters
+	// afterwards, available() will be >= n
+	// NOTE: should be followed by calling added()
+	char* ensure(int n);
+
+	// does ensure(n) and then added(n)
+	char* alloc(int n);
+
+	void add(char c);
+	void add(const char* s, int n);
+	void add(const gcstring& s);
+
+	void remove(int n);
+	int size()
+		{ return used; }
+	char* buffer()
+		{ return buf; }
+	void added(int n)
+		{ used += n; }
+	void clear()
+		{ used = 0; }
+	int available()
+		{ return capacity - used; }
+	char* bufnext()
+		{ return buf + used; }
+	char* str(); // no alloc
+	gcstring gcstr(); // no alloc
+private:
+	int capacity;
+	char* buf;
+	int used;
+	};
+
+#endif
