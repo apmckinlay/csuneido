39f6e95980875d7e0f82a32d94e398a2c15fe9c3
Andrew McKinlay <apmckinlay@gmail.com>
2013-08-12 22:02:31 +0000

    updated
    git-svn-id: svn://svn.code.sf.net/p/suneido/svn/trunk@1221 8ef86ec9-3220-0410-98d7-ddd3c8784f88

--- snapshot.bat
+++ snapshot.bat
@@ -1,16 +1,19 @@
 suneido -dump stdlib
 suneido -dump suneidoc
 suneido -dump imagebook
+suneido -dump mylib
+suneido -dump mybook
+suneido -dump mycontacts
 copy z:\software\suneido\translatelanguage.su
 call saveaway suneido.db
 suneido -load stdlib
 suneido -load suneidoc
 suneido -load imagebook
 suneido -load translatelanguage.su
-copy z:\software\suneido\beta\jsuneido.jar
+copy z:\software\suneido\jsuneido.jar
 del suneido%1.zip
-zip -9 suneido%1.zip suneido.exe suneido.db scilexer.dll libhpdf.dll clucene.dll hunspell.exe en_US.aff en_US.dic splash.bmp jsuneido.jar
+zip -9 suneido%1.zip suneido.exe suneido.db scilexer.dll libhpdf.dll clucene.dll hunspell.exe en_US.aff en_US.dic splash.bmp jsuneido.jar mylib.su mybook.su mycontacts.su
 del jsuneido.jar
-du -h suneido%1.zip
 del suneido.db
-unsave suneido.db
+call unsave suneido.db
+du -h suneido%1.zip
