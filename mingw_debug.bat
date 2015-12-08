97e79d05ab2adb3d9addd57517584c78299647fb
Andrew McKinlay <apmckinlay@gmail.com>
2015-12-08 13:41:11 -0600

    updated mingw batch files

--- mingw_debug.bat
+++ mingw_debug.bat
@@ -1,5 +1,5 @@
 @if not exist mingw_debug mkdir mingw_debug
 @if not exist gc6.5\mingw_debug mkdir gc6.5\mingw_debug
-doskey make=make -f makefile.mingw DEBUG=1 $*
-path %path%;c:\mingw\bin
+doskey make=mingw32-make -f makefile.mingw DEBUG=1 $*
+path c:\mingw\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;c:\program files (x86)\bin
 @title mingw debug
--- mingw_release.bat
+++ mingw_release.bat
@@ -1,5 +1,5 @@
 @if not exist mingw_release mkdir mingw_release
 @if not exist gc6.5\mingw_release mkdir gc6.5\mingw_release
-doskey make=make -f makefile.mingw $*
-path %path%;c:\mingw\bin
+doskey make=mingw32-make -f makefile.mingw $*
+path c:\mingw\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;c:\program files (x86)\bin
 @title mingw release
