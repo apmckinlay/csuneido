c212a374574175d9f097005db9c4768edabf788d
Andrew McKinlay <apmckinlay@gmail.com>
2016-01-06 16:18:16 -0600

    minor updates

--- snapshot.bat
+++ snapshot.bat
@@ -1,4 +1,5 @@
 suneido -dump stdlib
+suneido -dump Contrib
 suneido -dump suneidoc
 suneido -dump imagebook
 suneido -dump mylib
@@ -7,12 +8,13 @@ suneido -dump mycontacts
 copy z:\software\suneido\translatelanguage.su
 call saveaway suneido.db
 suneido -load stdlib
+suneido -load Contrib
 suneido -load suneidoc
 suneido -load imagebook
 suneido -load translatelanguage.su
 copy z:\software\suneido\jsuneido.jar
 del suneido%1.zip
-zip -9 suneido%1.zip suneido.exe suneido.db scilexer.dll libhpdf.dll clucene.dll hunspell.exe en_US.aff en_US.dic splash.bmp jsuneido.jar mylib.su mybook.su mycontacts.su
+zip -9 suneido%1.zip suneido.exe suneido.db scilexer.dll clucene.dll zlibwapi.dll hunspell.exe en_US.aff en_US.dic splash.bmp jsuneido.jar mylib.su mybook.su mycontacts.su
 del jsuneido.jar
 del suneido.db
 call unsave suneido.db
