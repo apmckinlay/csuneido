579a0d684e215310b6128ab9249bc7d4a02e101b
Andrew McKinlay <apmckinlay@gmail.com>
2016-01-08 11:39:00 -0600

    preparing new release

--- snapshot.bat
+++ snapshot.bat
@@ -12,10 +12,12 @@ suneido -load Contrib
 suneido -load suneidoc
 suneido -load imagebook
 suneido -load translatelanguage.su
-copy z:\software\suneido\jsuneido.jar
+copy \dev\jsuneido\jsuneido.jar
+copy \dev\jsuneido\lib\lucene*.jar
 del suneido%1.zip
-zip -9 suneido%1.zip suneido.exe suneido.db scilexer.dll clucene.dll zlibwapi.dll hunspell.exe en_US.aff en_US.dic splash.bmp jsuneido.jar mylib.su mybook.su mycontacts.su
+zip -9 suneido%1.zip suneido.exe suneido.db scilexer.dll clucene.dll zlibwapi.dll hunspell.exe en_US.aff en_US.dic splash.bmp jsuneido.jar lucene*.jar mylib.su mybook.su mycontacts.su release.html
 del jsuneido.jar
+del lucene*.jar
 del suneido.db
 call unsave suneido.db
 du -h suneido%1.zip
