68daef130c97f95d6094757743d55e9795b11a22
Andrew McKinlay <apmckinlay@gmail.com>
2017-01-23 15:48:53 -0700

    added gcstring.has

--- gcstring.h
+++ gcstring.h
@@ -4,18 +4,18 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
  * This file is part of Suneido - The Integrated Application Platform
  * see: http://www.suneido.com for more information.
- * 
- * Copyright (c) 2000 Suneido Software Corp. 
+ *
+ * Copyright (c) 2000 Suneido Software Corp.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation - version 2. 
+ * as published by the Free Software Foundation - version 2.
  *
  * This program is distributed in the hope that it will be
  * useful, but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
  * PURPOSE.  See the GNU General Public License in the file COPYING
- * for more details. 
+ * for more details.
  *
  * You should have received a copy of the GNU General Public
  * License along with this program; if not, write to the Free
@@ -58,7 +58,7 @@ public:
 
 	// CAUTION: constructor that doesn't allocate
 	// WARNING: p2[n2] must be valid, ie. p2 must be one bigger than n2 !!!
-	gcstring(size_t n2, const char* p2) 
+	gcstring(size_t n2, const char* p2)
 		: n(n2), p(n2 == 0 ? empty_buf : const_cast<char*>(p2))
 		{ }
 
@@ -92,13 +92,17 @@ public:
 	int findlast(const gcstring& x, size_t pos = 0) const;
 	int find(const char* s, size_t pos = 0) const
 		{ return find(gcstring(strlen(s), s), pos); }
+	bool has(const char* s) const
+		{ return -1 != find(s); }
+	bool has(const gcstring& s) const
+		{ return -1 != find(s); }
 
 	gcstring substr(size_t i, int len = -1) const;
 	gcstring trim() const;
 
 	bool has_prefix(const gcstring& s, size_t pos = 0) const;
 	bool has_suffix(const gcstring& s) const;
-	
+
 	gcstring to_heap();
 
 protected:
@@ -132,21 +136,21 @@ inline bool operator!=(const char* x, const gcstring& y)
 	{ return ! (x == y); }
 inline bool operator!=(const gcstring& x, const gcstring& y)
 	{ return ! (x == y); }
-	
+
 inline gcstring operator+(const char* s, const gcstring& t)
 	{
 	gcstring x(strlen(s), s);
-	return x += t; 
+	return x += t;
 	}
 inline gcstring operator+(const gcstring& s, const char* t)
-	{ 
+	{
 	gcstring x(s);
 	return x += gcstring(strlen(t), t);
 	}
 inline gcstring operator+(const gcstring& s, const gcstring& t)
-	{ 
+	{
 	gcstring x(s);
-	return x += t; 
+	return x += t;
 	}
 
 bool operator<(const gcstring& x, const gcstring& y);
