3528559424530f7a61fc105d3a11f3d12b9e5982
Andrew McKinlay <apmckinlay@gmail.com>
2017-06-12 16:31:43 -0600

    testing compiling with clang

---
+++ clang.bat
@@ -0,0 +1,5 @@
+@if not exist clang mkdir clang
+@if not exist gc6.5\clang mkdir gc6.5\clang
+doskey make=\mingw\bin\mingw32-make -f makefile.clang DEBUG=1 OUTPUT=clang $*
+path C:\Program Files (x86)\LLVM\bin;c:\mingw\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;c:\program files (x86)\bin
+@title clang
