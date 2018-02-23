a4a2ab719d13a17bafb677f5fa4340502d381011
Andrew McKinlay <apmckinlay@gmail.com>
2018-02-22 19:19:45 -0600

    new debug output

---
+++ dbg.cpp
@@ -0,0 +1,37 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+ * This file is part of Suneido - The Integrated Application Platform
+ * see: http://www.suneido.com for more information.
+ *
+ * Copyright (c) 2000 Suneido Software Corp.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation - version 2.
+ *
+ * This program is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License in the file COPYING
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include "dbg.h"
+#include "win.h"
+
+OstreamStr& osdbg_()
+	{
+	static OstreamStr oss;
+	return oss;
+	}
+
+void dbg_()
+	{
+	OutputDebugStringA(osdbg_().str());
+	OutputDebugStringA("\r\n");
+	osdbg_().clear();
+	}
