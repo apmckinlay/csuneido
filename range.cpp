6748ac26a36039f9f4ab09b5f8ca4be32f2c0f5e
Andrew McKinlay <apmckinlay@gmail.com>
2018-08-17 17:21:11 -0600

    remove Slice & Substr from lang_sub port test

--- range.cpp
+++ range.cpp
@@ -49,13 +49,14 @@ PORTTEST(lang_range) {
 	Value expected(new SuString(args[3]));
 	Value result = s.rangeTo(from, to);
 	if (result != expected)
-		return OSTR("got: " << result);
+		return OSTR("string got: " << result);
 
 	SuObject* list = stringToCharList(args[0]);
 	Value expectedList = stringToCharList(args[3]);
 	result = list->rangeTo(from, to);
 	if (result != expectedList)
-		return OSTR("got: " << result);
+		return OSTR("object got: " << result);
+
 	return nullptr;
 }
 
@@ -69,33 +70,13 @@ PORTTEST(lang_sub) {
 	Value expected(new SuString(args[last]));
 	Value result = s.rangeLen(org, len);
 	if (result != expected)
-		return OSTR("expected: " << expected << "got: " << result);
-
-	List<gcstring> args2;
-	args2.add(args[0]).add("Substr");
-	for (int i = 1; i < args.size(); ++i)
-		args2.add(args[i]);
-	List<bool> str2;
-	str2.add(str[0]).add(true);
-	for (int i = 1; i < args.size(); ++i)
-		str2.add(str[i]);
-	if (auto err = pt_method(args2, str2))
-		return OSTR("Substr " << err);
+		return OSTR("string got: " << result);
 
 	SuObject* list = stringToCharList(args[0]);
 	Value expectedList = stringToCharList(args[last]);
 	result = list->rangeLen(org, len);
 	if (result != expectedList)
-		return OSTR("got: " << result);
-
-	args2[0] = OSTR(list);
-	str2[0] = false;
-	args2[1] = "Slice";
-	last = args2.size() - 1;
-	args2[last] = OSTR(expectedList);
-	str2[last] = false;
-	if (auto err = pt_method(args2, str2))
-		return OSTR("Slice " << err);
+		return OSTR("object got: " << result);
 
 	return nullptr;
 }
