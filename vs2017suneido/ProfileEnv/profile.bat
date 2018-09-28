486f1c45ef6f56715d1129fb7caa00c7cf844426
Andrew McKinlay <apmckinlay@gmail.com>
2018-09-28 12:34:41 -0600

    simplify profile guided optimization

--- vs2017suneido/ProfileEnv/profile.bat
+++ vs2017suneido/ProfileEnv/profile.bat
@@ -1,26 +1,6 @@
-@echo off
-SET testDir=%1
-SET profileDir=%2
-IF EXIST %profileDir%\vs2017suneido.exe (
-	copy %profileDir%\vs2017suneido.exe %testDir%\suneido.exe
-)
-cd %testDir%
-
-echo EXE compact: START
-start /w suneido.exe -compact
-echo EXE compact: END
-
-echo Suneido setup: START
-start /W suneido.exe setup
-echo Suneido setup: END
-
-for %%f in (*.go) do (
-	echo %%~nf: START
-	start /W suneido.exe %%~f
-	echo %%~nf: END
-)
-IF EXIST %testDir%\*.pgc (
-	move %testDir%\*.pgc %profileDir%
-)
-
-exit
+cd %1
+echo Profiling...
+del %2\*.pgc 2>nul
+start /w %2\vs2017suneido.exe -u -t
+start /w %2\vs2017suneido.exe -compact
+start /w %2\vs2017suneido.exe test.go
